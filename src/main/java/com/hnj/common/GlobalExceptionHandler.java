8233142a5f3a8e1fe43aa4b3d70eed4111244a51
huangnj <4127@hnj.com>
2022-08-14 12:04:59 +0800

    修复员工提交时的bug

--- src/main/java/com/hnj/common/GlobalExceptionHandler.java
+++ src/main/java/com/hnj/common/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.hnj.common;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,8 @@ import java.sql.SQLIntegrityConstraintViolationException;
 @RestControllerAdvice(annotations = {RestController.class, Controller.class})
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
-    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+    @ExceptionHandler(DuplicateKeyException.class)
+    public R<String> exceptionHandler(DuplicateKeyException ex){
         if(ex.getMessage().contains("Duplicate entry")){
             String[] split=ex.getMessage().split(" ");
             String msg=split[2]+"已存在！";
@@ -31,5 +32,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(FileNotFoundException.class)
     public void exceptionHandler(FileNotFoundException ex){
         log.info("文件不存在！");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public R<String> exceptionHandler(Exception ex){
+        ex.printStackTrace();
+        return R.error("系统错误");
+
     }
 }
